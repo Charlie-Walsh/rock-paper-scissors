48e91d8802e661e8fc03d16de493b5eeb2c51a97
Charlie-Walsh <charlie.walsh.141@gmail.com>
2022-06-17 23:15:25 +0000

    testing started for TDD

---
+++ assets/tests/game.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+const { game } = require("../js/game");
+
+beforeEach(() => {
+    const fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game keys exist", () => {
+    test("does playerScore exist in game", () => {
+        expect(game.userScore).toBe(true);
+    });
+});
