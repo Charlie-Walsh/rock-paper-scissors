7feea67f54085d0059e33d0501ba88ddf94f288f
Charlie-Walsh <charlie.walsh.141@gmail.com>
2022-06-18 23:08:55 +0000

    items still null

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,36 +1,55 @@
-const game = {
-    playerScore: 0,
-    computerScore: 0,
-    playerScoreSpan: document.getElementById("wins"),
-    computerScoreSpan: document.getElementById("losses"),
-    scoreBoard: document.querySelector("#score-board"),
-    whoWon: document.getElementById("who-won"),
-    rock: document.getElementById("rock"),
-    paper: document.getElementById("paper"),
-    scissors: document.getElementById("scissors"),
-    computerChoices: ['r', 'p', 's'],
-    computerChoice: [],
-    playerChoice: [],
+document.addEventListener("DOMContentLoaded", () => {
+
+    const game = {
+        playerScore: 0,
+        computerScore: 0,
+        playerScoreSpan: document.getElementById("wins"),
+        computerScoreSpan: document.getElementById("losses"),
+        scoreBoard: document.querySelector("#score-board"),
+        whoWon: document.getElementById("who-won"),
+        rock: document.getElementById("rock"),
+        paper: document.getElementById("paper"),
+        scissors: document.getElementById("scissors"),
+        computerChoices: ['r', 'p', 's'],
+        computerChoice: [],
+        playerChoice: [],
+        };
+
+    function getComputerChoice() {
+        game.computerChoice = [];
+        game.computerChoice.push(game.computerChoices[Math.floor(Math.random() * 3)]);
+        };
+
+    function rps() {
+        getComputerChoice();
+        const compChoice = game.computerChoice[0];
+        const plyrChoice = game.playerChoice[0];
+        if (plyrChoice == "r") {
+            if (compChoice == "s") {
+                game.playerScore++;
+            } else if (compChoice == "p") {
+                game.computerScore++;
+            };
+        };
+        console.log(game.playerScore);
+        console.log(game.computerScore);
     };
 
-function getComputerChoice() {
-    game.computerChoice = [];
-    game.computerChoice.push(game.computerChoices[Math.floor(Math.random() * 3)]);
-    };
+    function getPlayerChoice() {
+        game.rock.addEventListener('click', function() {
+            game.playerChoice.push(['r']);
+            console.log(game.playerChoice)
+        });
+        game.paper.addEventListener('click', function() {
+            game.playerChoice.push(['p']);
+            rps()
+        });
+        game.scissors.addEventListener('click', function() {
+            game.playerChoice.push(['s']);
+            rps()
+        });
+        };
 
-function getPlayerChoice() {
-    game.rock.addEventListener('click', function() {
-        game.playerChoice.push(['r']);
-        game.rock.setAttribute("data-listener", "true");
-    });
-    game.paper.addEventListener('click', function() {
-        game.playerChoice.push(['p']);
-        game.paper.setAttribute("data-listener", "true");
-    });
-    game.scissors.addEventListener('click', function() {
-        game.playerChoice.push(['s']);
-        game.scissors.setAttribute("data-listener", "true");
-    });
-    };
+    module.exports = {game, getComputerChoice, getPlayerChoice}
 
-module.exports = {game, getComputerChoice, getPlayerChoice}
+});
